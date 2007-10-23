c8bb8ab8d89943076406993c1a94e43d729e273f
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2007-10-23 11:44:49 +0000

    Removed Version V2.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches@7077 735d13b6-9817-0410-8766-e36946ffe9aa

--- v2/cm1/conf.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-	// DO NOT REMOVE OR CHANGE THESE 3 LINES:
-define('TYPO3_MOD_PATH', '../typo3conf/ext/tx_l10nmgr/cm1/');
-$BACK_PATH='../../../../typo3/';
-$MCONF['name']='xMOD_tx_l10nmgr_cm1';
-?>
