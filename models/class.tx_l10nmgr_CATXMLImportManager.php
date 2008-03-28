ca43d6901593a7ebe9f6965e93db54061167de1e
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-03-28 09:52:26 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v3@8726 735d13b6-9817-0410-8766-e36946ffe9aa

--- models/class.tx_l10nmgr_CATXMLImportManager.php
+++ models/class.tx_l10nmgr_CATXMLImportManager.php
@@ -28,25 +28,43 @@ require_once(t3lib_extMgm::extPath('l10nmgr').'models/tools/class.tx_l10nmgr_xml
 /**
  * Function for managing the Import of CAT XML
  *
- * @author	Daniel Pötzinger <ext@aoemedia.de>
+ * @author	Daniel Poetzinger <ext@aoemedia.de>
  *
  * @package TYPO3
  * @subpackage tx_l10nmgr
  */
 class tx_l10nmgr_CATXMLImportManager {
-	var $file;	//filepath with XML
-	
-	var $xmlNodes;		//parsed XML
-	var $headerData;	//headerData of the XML
-	var $sysLang;			//selected import language (for check purposes)
-	
-	var $_errorMsg=array();  //accumulated errormessages
-	
+
+	/**
+	 * @var	string		$file		filepath with XML
+	 */
+	var $file = '';;
+
+	/**
+	 * @var	string		$xmlNodes		parsed XML
+	 */
+	var $xmlNodes = '';
+
+	/**
+	 * @var	string		$headerData		headerData of the XML
+	 */
+	var $headerData = '';
+
+	/**
+	 * @var	integer		$sysLang		selected import language (for check purposes)
+	 */
+	var $sysLang;
+
+	/**
+	 * @var	array		$_errorMsg		accumulated errormessages
+	 */
+	var $_errorMsg = array();
+
 	function tx_l10nmgr_CATXMLImportManager($file,$sysLang) {
-		$this->sysLang=$sysLang;
-		$this->file=$file;
+		$this->sysLang = $sysLang;
+		$this->file    = $file;
 	}
-	
+
 	function parseAndCheckXMLFile() {
 		$fileContent = t3lib_div::getUrl($this->file);
 		$this->xmlNodes = t3lib_div::xml2tree(str_replace('&nbsp;',' ',$fileContent),3);	// For some reason PHP chokes on incoming &nbsp; in XML!
@@ -59,54 +77,51 @@ class tx_l10nmgr_CATXMLImportManager {
 			$this->_errorMsg[]='could not found header data in XML!';
 			return false;
 		}
-		$this->_setHeaderData($headerInformationNodes);		
-		if ($this->_isIncorrectXMLFile()) {			
+		$this->_setHeaderData($headerInformationNodes);
+		if ($this->_isIncorrectXMLFile()) {
 			return false;
-		}		
+		}
 	}
+
 	function getErrorMessages() {
 		return implode('<br />',$this->_errorMsg);
 	}
-	
+
 	function &getXMLNodes() {
 		return $this->xmlNodes;
 	}
 	
 	function _isIncorrectXMLFile() {
-		$error=array();
+		$error = array();
+
 		if (!isset($this->headerData['FormatVersion']) || $this->headerData['FormatVersion'] !=L10NMGR_FILEVERSION) {
-			$error[]='Incorrect Version of the Format: '.$this->headerData['FormatVersion'].' (required: '.L10NMGR_FILEVERSION.')';
+			$error[] = 'Incorrect Version of the Format: '.$this->headerData['FormatVersion'].' (required: '.L10NMGR_FILEVERSION.')';
 		}
 		if (!isset($this->headerData['workspaceId']) || $this->headerData['workspaceId'] !=$GLOBALS['BE_USER']->workspace) {
-			$error[]='Export was taken from a diffrent Workspace, please import in this workspace to avoid problems: Current'.$GLOBALS['BE_USER']->workspace.' (required: '.$this->headerData['workspaceId'].')';
+			$error[] = 'Export was taken from a diffrent Workspace, please import in this workspace to avoid problems: Current'.$GLOBALS['BE_USER']->workspace.' (required: '.$this->headerData['workspaceId'].')';
 		}
 		if (!isset($this->headerData['sysLang']) || $this->headerData['sysLang'] !=$this->sysLang) {
-			$error[]='Export was taken from a diffrent Language, please select correct language above! current:'.$this->sysLang.' (required: '.$this->headerData['sysLang'].')';
+			$error[] = 'Export was taken from a diffrent Language, please select correct language above! current:'.$this->sysLang.' (required: '.$this->headerData['sysLang'].')';
 		}
 		if (count($error)>0) {
-			$this->_errorMsg=array_merge($this->_errorMsg,$error);
+			$this->_errorMsg = array_merge($this->_errorMsg,$error);
 			return true;
 		}
 		return false;
 	}
-	
-	function _setHeaderData($headerInformationNodes) {	
+
+	function _setHeaderData($headerInformationNodes) {
+
 		if (!is_array($headerInformationNodes)) {
 			return;
-		}	
+		}
 		foreach ($headerInformationNodes as $k=>$v) {
 			$this->headerData[$k]=$v[0]['values'][0];
 		}
 	}
-	
 }
 
-
-
-
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/models/class.tx_l10nmgr_translationDataFactory.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/models/class.tx_l10nmgr_translationDataFactory.php']) {
 	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/models/class.tx_l10nmgr_translationDataFactory.php']);
 }
-
-
 ?>
