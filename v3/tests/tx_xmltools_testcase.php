8b2c06540af7b776f5afa6c626d201fad142cdde
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-17 09:17:57 +0000

    add some test on im/export string representation - bug wich add's highlighted span tag to dead links
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches@8928 735d13b6-9817-0410-8766-e36946ffe9aa

--- v3/tests/tx_xmltools_testcase.php
+++ v3/tests/tx_xmltools_testcase.php
@@ -22,83 +22,152 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-/**
- * Test case for checking the xmltools class
- *
- * 
- *
- * @author	Daniel Pötzinger
- */
-
-
 require_once(t3lib_extMgm::extPath('l10nmgr').'models/tools/class.tx_l10nmgr_xmltools.php');
 require_once (PATH_t3lib.'class.t3lib_tcemain.php');
 
+
+/**
+ * Test case for checking the xmltools class
+ *
+ * @author  Daniel Poetzinger <poetzinger@aoemedia.de>
+ * @author  Michael Klapper <klapper@aoemedia.de>
+ */
 class tx_xmltools_testcase extends tx_phpunit_testcase {
-	
-	public function test_isXMLString()
-	{		
-		//prepare testdatas
-		$_fixture_noXML='<a>my test<p>test</p>';
-		$_fixture_noXML2='my test & du';
-		$_fixture_validXML='<a>my test</a><p>test</p><strong>&amp;<i></i><br /></strong>';		
-		//do the tests
-		$xmlTools=t3lib_div::makeInstance('tx_l10nmgr_xmltools');	      
-    $this->assertFalse($xmlTools->isValidXMLString($_fixture_noXML), "invalid xml is detected as XML!");    
-    $this->assertFalse($xmlTools->isValidXMLString($_fixture_noXML2), "invalid xml 2 is detected as XML!");    
-    $this->assertTrue($xmlTools->isValidXMLString($_fixture_validXML), "XML should be valid");   
-    
+
+	/**
+	 * @var 	tx_l10nmgr_xmltools 		$XMLtools
+	 */
+	protected $XMLtools = null;
+
+
+	/**
+	 * setUp
+	 * Create the XMLtools object 
+	 * 
+	 * @return void
+	 */
+	public function setUp() {
+
+		$this->XMLtools = t3lib_div::makeInstance('tx_l10nmgr_xmltools');
 	}
 
-	public function test_simpleTransformationTest()
-	{
+	/**
+	 * test_isXMLString
+	 *
+	 * @return void
+	 */
+	public function test_isXMLString() {
+
+		//prepare testdatas
+		$_fixture_noXML    = '<a>my test<p>test</p>';
+		$_fixture_noXML2   = 'my test & du';
+		$_fixture_validXML = '<a>my test</a><p>test</p><strong>&amp;<i></i><br /></strong>';
+
+		//do the tests
+		$this->assertFalse($this->XMLtools->isValidXMLString($_fixture_noXML), "invalid xml is detected as XML!");
+		$this->assertFalse($this->XMLtools->isValidXMLString($_fixture_noXML2), "invalid xml 2 is detected as XML!");
+		$this->assertTrue($this->XMLtools->isValidXMLString($_fixture_validXML), "XML should be valid");
+	}
+
+	/**
+	 * test_simpleTransformationTest
+	 *
+	 * @return void
+	 */
+	public function test_simpleTransformationTest() {
 		//prepare testdata
 		$fixtureRTE='<link 3>my link</link><strong>strong text</strong>'."\n";
-		$fixtureRTE.='test';		
+		$fixtureRTE.='test';
+
 		//do the test:
-		$xmlTools=t3lib_div::makeInstance('tx_l10nmgr_xmltools');										
-		$transformed=$xmlTools->XML2RTE($xmlTools->RTE2XML($fixtureRTE));		
-    $this->assertEquals($transformed, $fixtureRTE, "transformationresult:".$transformed." is not equal to source.");
+		$transformed=$this->XMLtools->XML2RTE($this->XMLtools->RTE2XML($fixtureRTE));
+		$this->assertEquals($transformed, $fixtureRTE, "transformationresult:".$transformed." is not equal to source.");
 	}
-	
-	public function test_transformationLinkTagTest()
-	{
+
+	/**
+	 * test_transformationLinkTagTest
+	 *
+	 * @return void
+	 */
+	public function test_transformationLinkTagTest() {
 		//prepare testdata
 		$fixtureRTE='<link 3 target class "name">my link</link><strong>strong text</strong>'."\n";
 		$fixtureRTE.='test';
-		
+
 		//do the test:
-		$xmlTools=t3lib_div::makeInstance('tx_l10nmgr_xmltools');										
-		$transformed=$xmlTools->XML2RTE($xmlTools->RTE2XML($fixtureRTE));
-		
-    $this->assertEquals($transformed, $fixtureRTE, "transformationresult:".$transformed." is not equal to source.");    
-    
-    
+		$transformed=$this->XMLtools->XML2RTE($this->XMLtools->RTE2XML($fixtureRTE));
+
+		$this->assertEquals($transformed, $fixtureRTE, "transformationresult:".$transformed." is not equal to source.");
 	}
-	
-	
-	public function test_transformationEntityTest()
-	{
+
+
+	/**
+	 * test_transformationEntityTest
+	 *
+	 * @return void
+	 */
+	public function test_transformationEntityTest()	{
 		//prepare testdata
 		$fixtureRTE='& &amp; &nbsp; ich&du';
-		
-		$xmlTools=t3lib_div::makeInstance('tx_l10nmgr_xmltools');										
-		$transfxml=$xmlTools->RTE2XML($fixtureRTE);
-		
-		//test if entities and & were transformed correct
-		$this->assertEquals($transfxml, '<p>&amp; &amp;amp; &nbsp; ich&amp;du</p>', "entities transformed incorrect"); 
-		
-		//do the test:		
-		$transformed=$xmlTools->XML2RTE($transfxml);
-		
-    $this->assertEquals($transformed, $fixtureRTE, "transformationresult is not equal to source.");    
-    
-    
-	}
-	
-	
-	
-	
 
+		$transfxml=$this->XMLtools->RTE2XML($fixtureRTE);
+
+		//test if entities and & were transformed correct
+		$this->assertEquals($transfxml, '<p>&amp; &amp;amp; &nbsp; ich&amp;du</p>', "entities transformed incorrect");
+
+		//do the test:
+		$transformed=$this->XMLtools->XML2RTE($transfxml);
+
+		$this->assertEquals($transformed, $fixtureRTE, "transformationresult is not equal to source.");
+	}
+
+
+	/**
+	 * test_keepXHTMLValidBRTest
+	 *
+	 * @return void
+	 */
+	public function test_keepXHTMLValidBRTest() {
+
+		// prepare the test data
+		$fixtureRTE = 'here coms some .. 8747()/=<="($<br />';
+
+		$fixtureXML  = $this->XMLtools->RTE2XML($fixtureRTE);
+		$transformed = $this->XMLtools->XML2RTE($fixtureXML);
+		
+		$this->assertEquals($transformed, $fixtureRTE, 'transformation result is not as expected ');
+	}
+
+
+	/**
+	 * testr_keepXHMLValidBRInnerList 
+	 * 
+	 * @return void
+	 */
+	public function testr_keepXHMLValidBRInnerList() {
+
+		//prepare the test data
+		$fixtureRTE = '<ul><li>    Sign on with a single user name and password to simplify user management and support    </li><li> Easily share individual applications and documents with the click of a mouse    </li><li> Simplify meeting participation with callbacks and 800 numbers through our integrated telephony and audio<br /><br /> </li></ul>';
+
+		$fixtureXML   = $this->XMLtools->RTE2XML($fixtureRTE);
+		$transformed  = $this->XMLtools->XML2RTE($fixtureXML);
+
+		$this->assertEquals($transformed, $fixtureRTE, 'transformation result is not as expected ');
+	}
+
+	/**
+	 * test_removeDeadLinkHandlingTest
+	 *
+	 * @return void
+	 */
+	public function test_removeDeadLinkHandlingTest() {
+
+		// prepare testdata
+		$fixtureRTE  = 'here comes some ... <link 92783928>this is my link</link>';
+
+		$transformed = $this->XMLtools->XML2RTE($this->XMLtools->RTE2XML($fixtureRTE));
+		
+		$this->assertEquals($transformed, $fixtureRTE, 'transformation result is not as expected ');
+	}
 }
 ?>
