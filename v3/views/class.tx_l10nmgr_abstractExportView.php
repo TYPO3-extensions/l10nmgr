8744ca87b55d0fd2fd41e8ba64f4996e23fc59ee
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-04-24 08:06:20 +0000

    Path to uploads directory was using a relative path which leads to problems on different installations. Removing the relative part of the path and adding the PHP constant PATH_site solves the problem.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches@8991 735d13b6-9817-0410-8766-e36946ffe9aa

--- v3/views/class.tx_l10nmgr_abstractExportView.php
+++ v3/views/class.tx_l10nmgr_abstractExportView.php
@@ -3,9 +3,9 @@
 *  Copyright notice
 *
 *  (c) 2006 Kasper Skårhøj <kasperYYYY@typo3.com>
-* 
+*
 *  @author	Fabian Seltmann <fs@marketing-factory.de>
-* 
+*
 *  All rights reserved
 *
 *  This script is part of the TYPO3 project. The TYPO3 project is
@@ -27,7 +27,7 @@
 
 
 /**
-* abstrakt Base class for rendering the export or htmllist of a l10ncfg 
+* abstrakt Base class for rendering the export or htmllist of a l10ncfg
 **/
 class tx_l10nmgr_abstractExportView {
 
@@ -40,53 +40,53 @@ class tx_l10nmgr_abstractExportView {
 	 * @var	integer		$sysLang		The sys_language_uid of language to export
 	 */
 	var $sysLang;
-	
+
 	/**
 	*	 flags for controlling the fields which should render in the output:
-	*/	
+	*/
 	var $modeOnlyChanged=FALSE;
 	var $modeOnlyNew=FALSE;
-	
-	function __construct($l10ncfgObj, $sysLang) {	
+
+	function __construct($l10ncfgObj, $sysLang) {
 		$this->sysLang = $sysLang;
-		$this->l10ncfgObj = $l10ncfgObj;		
+		$this->l10ncfgObj = $l10ncfgObj;
 	}
-	
+
 	function getExportType() {
 		return $exportType;
 	}
 
 	function setModeOnlyChanged() {
-		
+
 		$this->modeOnlyChanged=TRUE;
 	}
-	function setModeOnlyNew() {		
+	function setModeOnlyNew() {
 		$this->modeOnlyNew=TRUE;
 	}
-	
+
 	/**
 	 * create a filename to save the File
-	 */ 
+	 */
 	function getLocalFilename(){
 		if($this->exportType == '0'){
-			$fileType = 'excel_export_'; 
+			$fileType = 'excel_export_';
 		}else{
 			$fileType = 'catxml_export_';
 		}
 		return $fileType.$this->sysLang.'_'.date('dmy-Hi').'.xml';
 	}
-	
+
 	/**
 	 * save the information of the export in the database table 'tx_l10nmgr_sava_data'
-	 */ 
+	 */
 	function saveExportInformation(){
-			
+
 		// get current date
 		$date = time();
-		
+
 		//To-Do get source language if another than default is selected
 		$sourceLanguageId=0;
-		
+
 		// query to insert the data in the database
 		$field_values = array(						'source_lang' => $sourceLanguageId,
 													'translation_lang' => $this->sysLang,
@@ -99,21 +99,21 @@ class tx_l10nmgr_abstractExportView {
 													'cruser_id' => $this->l10ncfgObj->getData('cruser_id'),
 													'filename' => $this->getLocalFilename(),
 													'exportType' => $this->exportType);
-													
+
 		$res = $GLOBALS['TYPO3_DB']->exec_INSERTquery('tx_l10nmgr_exportdata', $field_values);
-		
+
 		#t3lib_div::debug();
 		return $res;
 	}
-	
+
 	/**
 	 * checks if an export exists
 	 *
 	 */
 	function checkExports(){
-		
-		$sysLang = $this->sysLang;	
-		
+
+		$sysLang = $this->sysLang;
+
 		$result = $GLOBALS['TYPO3_DB']->exec_SELECTquery('l10ncfg_id,exportType,translation_lang','tx_l10nmgr_exportdata','l10ncfg_id ='.$this->l10ncfgObj->getData('uid').' AND exportType ='.$this->exportType.' AND translation_lang ='.$sysLang);
 		$numRows = mysql_num_rows($result);
 		if ($numRows > 0){
@@ -122,15 +122,15 @@ class tx_l10nmgr_abstractExportView {
 			return TRUE;
 		}
 	}
-	
+
 	/**
 	 *  save the exported files in the file /uploads/tx_10lnmgr/saved_files/
 	 */
 	function saveExportFile($fileContent){
-		$fileExportName = '../../../../uploads/tx_l10nmgr/saved_files/'.$this->getLocalFilename();
+		$fileExportName = PATH_site . 'uploads/tx_l10nmgr/saved_files/'.$this->getLocalFilename();
 		file_put_contents($fileExportName,$fileContent);
 	}
-	
+
 	/**
 	 * Diff-compare markup
 	 *
@@ -143,9 +143,9 @@ class tx_l10nmgr_abstractExportView {
 		$t3lib_diff_Obj = t3lib_div::makeInstance('t3lib_diff');
 		return $t3lib_diff_Obj->makeDiffDisplay($old,$new);
 	}
-	
+
 }
-	
+
 if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS['TYPO3_MODE']['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_abstractExportView.php'])	{
 	include_once($TYPO3_CONF_VARS['TYPO3_MODE']['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_abstractExportView.php']);
 }
