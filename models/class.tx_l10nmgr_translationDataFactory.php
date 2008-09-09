a3b5e60b5e728417a7232af71024c74466729c87
dazi001 <dazi001@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-09-09 19:12:23 +0000

    Fixed problems with tags in non RTE-fields
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v3@11848 735d13b6-9817-0410-8766-e36946ffe9aa

--- models/class.tx_l10nmgr_translationDataFactory.php
+++ models/class.tx_l10nmgr_translationDataFactory.php
@@ -139,28 +139,36 @@ class tx_l10nmgr_translationDataFactory {
 							//Substitute &amp; with & and <br/> with <br>
 							$row['XMLvalue']=str_replace('&amp;','&',$row['XMLvalue']);
 							$row['XMLvalue']=str_replace('<br/>','<br>',$row['XMLvalue']);
+							$row['XMLvalue']=str_replace('<br />','<br>',$row['XMLvalue']);
 							$row['values'][0]=str_replace('&amp;','&',$row['values'][0]);
 							$row['values'][0]=str_replace('<br/>','<br>',$row['values'][0]);
+							$row['values'][0]=str_replace('<br />','<br>',$row['values'][0]);
 
 							//check if $row['values'][0] is beginning of $row['XMLvalue']
-							//print "V0: ".$row['values'][0]."<br>";
-							//print "XML:".$row['XMLvalue']." |<br>";
+							//print "V0: ".$row['values'][0]."<br>\n";
+							//print "XML:".$row['XMLvalue']." |<br>\n";
 							$pattern = preg_replace('/\//i','\/',$row['values'][0]);
+							//print "Pattern: \"".$pattern."\"<br>\n";
+							$pattern2 = '/'.$pattern.'/';
 							$pattern = '/^'.$pattern.'/';
+							//print "Pattern: \"".$pattern."\"<br>\n";
+							//print "Pattern 2: \"".$pattern2."\"<br>\n";
 							if (preg_match($pattern, $row['XMLvalue'],$treffer)) {
-								//print "No TAG found!!!<br>";
-								//print $row['XMLvalue']."<br>";
+								//print "Start row[values][0] eq start row[XMLvalue]!!!<br>\n";
+								//print $row['XMLvalue']."<br>\n";
 								$translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = $row['XMLvalue'];
-							} elseif (preg_match('/<[^>]+>/i', $row['XMLvalue'])) {
-								//print "TAG found!!!<br>";
-								//print $row['XMLvalue']."<br>";
+							} elseif ((preg_match('/<[^>]+>/i', $row['XMLvalue'])) && (!preg_match($pattern2, $row['XMLvalue'],$treffer))) {
+								//print "TAG found in row[XMLvalue]!!!<br>\n";
+								//print "1: ".$row['XMLvalue']."<br>\n";
+								//print "2: ".$row['values'][0]."<br>\n";
 								$translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = $row['values'][0].$row['XMLvalue'];
 							} else {
-								//print "No TAG found!!!<br>";
-								//print $row['XMLvalue']."<br>";
+								//print "No TAG found in row[XMLvalue]!!!<br>\n";
+								//print $row['XMLvalue']."<br>\n";
 								$translation[$attrs['table']][$attrs['elementUid']][$attrs['key']] = $row['XMLvalue'];
 							}
-							//print "---<br>";
+							//print "IMPORT: ".$translation[$attrs['table']][$attrs['elementUid']][$attrs['key']]."<br/>\n";
+							//print "---<br>\n";
 							
 						}
 					}
