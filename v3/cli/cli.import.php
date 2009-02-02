8c249dd53061bc35557e173b29fef078ba0b8ea7
dazi001 <dazi001@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-02-02 16:49:02 +0000

    Bugfix in CLI importer
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches@16282 735d13b6-9817-0410-8766-e36946ffe9aa

--- v3/cli/cli.import.php
+++ v3/cli/cli.import.php
@@ -43,7 +43,7 @@ class tx_cliimport_cli extends t3lib_cli {
         $this->cli_help['name'] = 'Localization Manager importer';        
         $this->cli_help['synopsis'] = '###OPTIONS###';
         $this->cli_help['description'] = 'Class with import functionality for l10nmgr';
-        $this->cli_help['examples'] = '/.../cli_dispatch.phpsh l10nmgr import|importPreview|preview CATXML serverlink';
+        $this->cli_help['examples'] = '/.../cli_dispatch.phpsh l10nmgr_import import|importPreview|preview CATXML serverlink';
         $this->cli_help['author'] = 'Daniel Zielinski - L10Ntech.de, (c) 2008';
     }
 
