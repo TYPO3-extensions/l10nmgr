9ad18002544fa0b83dc5f60fbd3c10e36e38abb0
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-04 16:09:45 +0000

    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v4@19872 735d13b6-9817-0410-8766-e36946ffe9aa

--- tests/configuration/class.tx_l10nmgr_models_configuration_Configuration_testcase.php
+++ tests/configuration/class.tx_l10nmgr_models_configuration_Configuration_testcase.php
@@ -1,5 +1,5 @@
 <?php
-	// autoload the mvc 
+	// autoload the mvc
 if (t3lib_extMgm::isLoaded('mvc')) {
 	require_once(t3lib_extMgm::extPath('mvc').'common/class.tx_mvc_common_classloader.php');
 	tx_mvc_common_classloader::loadAll();
@@ -12,7 +12,7 @@ require_once(t3lib_extMgm::extPath('l10nmgr').'models/configuration/class.tx_l10
 
 /**
  * This class is used to test the functionallity of the l10nAccumulatedInformationsFactory class.
- * 
+ *
  * @author Timo Schmidt
  * @see tx_l10nmgr:l10nAccumulatedInformationFactory
  *
@@ -27,16 +27,16 @@ class tx_l10nmgr_models_configuration_configuration_testcase extends tx_phpunit_
 	public function setUp(){
 		$this->createDatabase();
 		$db = $this->useTestDatabase();
-		
-		$this->importExtensions(array('corefake','cms','l10nmgr','static_info_tables','templavoila'));		
+
+		$this->importExtensions(array('corefake','cms','l10nmgr','static_info_tables','templavoila'));
 	}
-	
+
 	public function tearDown(){
 		$this->cleanDatabase();
 		$this->dropDatabase();
-		$GLOBALS['TYPO3_DB']->sql_select_db(TYPO3_db);		
+		$GLOBALS['TYPO3_DB']->sql_select_db(TYPO3_db);
 	}
-	
+
 	/**
 	 * This method is used to load a FixtureL10NConfig
 	 *
@@ -45,16 +45,16 @@ class tx_l10nmgr_models_configuration_configuration_testcase extends tx_phpunit_
 	protected function getFixtureL10NConfig(){
 		$fixtureConfigRepository = new tx_l10nmgr_models_configuration_configurationRepository();
 		$fixtureConfig = $fixtureConfigRepository->findById(32);
-				
+
 		return $fixtureConfig;
 	}
-	
+
 	public function test_getPageIdsFromPageTree () {
 		$this->importDataSet(dirname(__FILE__). '/fixtures/canDeterminePageIdsFromPageTree.xml');
-		
+
 		$fixtureConfig = $this->getFixtureL10NConfig();
 		$pageCollection = $fixtureConfig->getExportPageIdCollection();
-		
+
 		$this->assertTrue(in_array(4711,$pageCollection->getArrayCopy()),'page could not be found in page collection');
 		$this->assertFalse(in_array(4715,$pageCollection->getArrayCopy()),'page could not be found in page collection');
 		//var_dump($pageCollection);
