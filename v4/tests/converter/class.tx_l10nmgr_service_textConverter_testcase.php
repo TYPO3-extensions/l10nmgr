c51a6fe8076262d2b1d4556f4986b8a72073774a
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-05-18 14:09:27 +0000

    Fix &amp; problem within the link-text
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches@20478 735d13b6-9817-0410-8766-e36946ffe9aa

--- v4/tests/converter/class.tx_l10nmgr_service_textConverter_testcase.php
+++ v4/tests/converter/class.tx_l10nmgr_service_textConverter_testcase.php
@@ -86,10 +86,11 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * The &nbsp; entity must be protected and can not be convertet to a simple " ".
 	 *
 	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_convertTextToXML() {
+	public function convertTextToXML() {
 		$fixtureRTE  = '& &amp; &nbsp; =< &auml;';
 		$expectedXML = '<p>&amp; &amp; &nbsp; =&lt; ä</p>';
 
@@ -106,10 +107,11 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * That means that the typolink tag "<link>" is transformed to the corresponding "<a>" and back again to the "<link>" tag.
 	 *
 	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_roundTransformationOfTypoLinkInlcudingNewlineCharacter() {
+	public function roundTransformationOfTypoLinkInlcudingNewlineCharacter() {
 		$fixtureText  = '<link 3>my link</link><strong>strong text</strong>'."\n" . 'test';
 		$expectedText = $fixtureText;
 
@@ -128,10 +130,11 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * The link parameter should be placed in the link correct and transformed them back to the typolinktag correct.
 	 *
 	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_roundTransformationOfTypoLinkWithFurtherParameterIncludingNewlineCharacter() {
+	public function roundTransformationOfTypoLinkWithFurtherParameterIncludingNewlineCharacter() {
 		$fixtureText  = '<link 3 target class "title text" name>>my link</link><strong>strong text</strong>'."\n" . 'test';
 		$expectedText = $fixtureText;
 
@@ -153,10 +156,11 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * - &auml;
 	 *
 	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_roundTransformationOfBasicEntities() {
+	public function roundTransformationOfBasicEntities() {
 		$fixtureText           = '& &amp; &nbsp; ich&du &auml;';
 		$expectedXML           = '<p>&amp; &amp; &nbsp; ich&amp;du ä</p>';
 		$roundTripExpectedText = '& & &nbsp; ich&du ä';
@@ -179,11 +183,12 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	/**
 	 * Verify that the retrieved content within div section are don't contains paragraph tags.
 	 *
-	 * @access publc
+	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_roundTransformationDontSetParagraphIntoDivElements() {
+	public function roundTransformationDontSetParagraphIntoDivElements() {
 		$fixtureText  = '<div id="lipsum"> Lorem ipsum dolor sit amet, consectetur </div>';
 		$expectedText = $fixtureText;
 
@@ -198,11 +203,34 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * Verify that the dead links will not removed while transformating
 	 *
 	 * @access public
+	 * @test
 	 * @author Michael Klapper <michael.klapper@aoemedia.de>
 	 * @return void
 	 */
-	public function test_roundTransformationKeepDeadLink() {
+	public function roundTransformationKeepDeadLink() {
 		$fixtureRTE  = 'here comes some ... <link 92783928>this is my link</link>';
+
+		$transformed = $this->TextConverter->toText (
+			$this->TextConverter->toXML($fixtureRTE)
+		);
+
+		$this->assertEquals (
+			$transformed,
+			$fixtureRTE,
+			'transformation result is not as expected'
+		);
+	}
+
+	/**
+	 * Verify that the '\xe2\x80\x94' is not touched
+	 *
+	 * @access public
+	 * @test
+	 * @author Michael Klapper <michael.klapper@aoemedia.de>
+	 * @return void
+	 */
+	public function roundTransformationOfHighLevelSign() {
+		$fixtureRTE  = chr(0xe2).chr(0x80).chr(0x94); /* "\xe2\x80\x94" */
 
 		$transformed = $this->TextConverter->toText (
 			$this->TextConverter->toXML($fixtureRTE)
@@ -219,10 +247,11 @@ class tx_l10nmgr_service_textConverter_testcase extends tx_phpunit_testcase {
 	 * Verify that a simple Contentelement with a bullet list can be transformed.
 	 *
 	 * @access public
+	 * @test
 	 * @author Timo Schmidt <timo.schmidt@aoemedia.de>
 	 * @return void
 	 */
-	public function test_transformBulletlistWithTypolinksAndEntity(){
+	public function transformBulletlistWithTypolinksAndEntity(){
 		$fixtureRTE 	= '<ul><li><link 25771 >WebEx Meeting Center</link></li><li><link 25611 >Who uses WebEx &amp; why?</link></li><li><link 24961 >Buy WebEx</link></li><li><link http://www.webex.com/go/live_demo >Live online demo</link></li><li><link http://www.webex.com/go/quick_tour 1050x700:resizable=0>Quick tour</link></li></ul>';
 		$expected		= '<ul><li><link 25771>WebEx Meeting Center</link></li><li><link 25611>Who uses WebEx & why?</link></li><li><link 24961>Buy WebEx</link></li><li><link http://www.webex.com/go/live_demo>Live online demo</link></li><li><link http://www.webex.com/go/quick_tour 1050x700:resizable=0>Quick tour</link></li></ul>';
 
