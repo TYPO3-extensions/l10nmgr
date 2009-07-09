db792316cde98f816132edda86489d2508b317b9
fabriziobranca <fabriziobranca@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-09 14:22:06 +0000

    removed exportDataRepositories' findByWhere method which overwrote the new findByWhere method from the mvc framework and adapted calls to it 8as the order of parameters has changed)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v4@22193 735d13b6-9817-0410-8766-e36946ffe9aa

--- models/exporter/class.tx_l10nmgr_models_exporter_exportDataRepository.php
+++ models/exporter/class.tx_l10nmgr_models_exporter_exportDataRepository.php
@@ -32,43 +32,18 @@ class tx_l10nmgr_models_exporter_exportDataRepository extends tx_mvc_ddd_typo3_a
 	 */
 	protected $objectClassName = 'tx_l10nmgr_models_exporter_exportData';
 
-	/**
-	 * 
-	 */
-	protected function findByWhere($where,$groupby=false,$orderby=false,$limit=false,$add_enable_fields=true) {
-		$queryParts = array ();
 
-		$select = '*';
-		$from   = $this->tableName;
-
-		$queryParts = tx_mvc_system_dbtools::buildQueryPartsArray (
-			$select,
-			$from,
-			$where,
-			$groupby,
-			$orderby,
-			$limit
-		);
-
-		if ($add_enable_fields) {
-			$queryParts ['WHERE'] .= $this->getEnableFieldsWhere();
-		}
-
-		$res = $this->getDatabase ()->exec_SELECT_queryArray($queryParts);
-
-		return $this->getCollectionFromRs($res);
-	}
 	/**
 	 * Returns a exportData objects with this stage in the history
 	 *
 	 * @param ArrayObject $state
 	 * @return ArrayObject
 	 */
-	public function findAllWithStateInHistory($state,$add_enable_fields = true) {
+	public function findAllWithStateInHistory($state, $add_enable_fields = true) {
 		//SELECT * FROM tx_l10nmgr_workflowstates s1 WHERE tstamp = ( SELECT MAX( s2.tstamp ) FROM `tx_l10nmgr_workflowstates` s2 WHERE s1.exportdata_id = s2.exportdata_id )
 
 		$where  = 'uid IN( SELECT DISTINCT exportdata_id FROM tx_l10nmgr_workflowstates WHERE state ='.tx_mvc_common_typo3::fullQuoteString($state).')';
-		return $this->findByWhere($where,false,false,false,$add_enable_fields);
+		return $this->findByWhere($where, $add_enable_fields, false, false, false);
 	}
 
 	/**
@@ -78,15 +53,15 @@ class tx_l10nmgr_models_exporter_exportDataRepository extends tx_mvc_ddd_typo3_a
 	 * @param boolean $add_enable_fields
 	 * @return ArrayObject
 	 */
-	public function findAllWithoutStateInHistory($state,$add_enable_fields = true) {
+	public function findAllWithoutStateInHistory($state, $add_enable_fields = true) {
 		$where  = 'uid NOT IN( SELECT DISTINCT exportdata_id FROM tx_l10nmgr_workflowstates WHERE state ='.tx_mvc_common_typo3::fullQuoteString($state).')';
-		return $this->findByWhere($where,false,false,false,$add_enable_fields);
+		return $this->findByWhere($where, $add_enable_fields, false, false, false);
 	}
 
 	/**
 	 * This method is used to find all exportData objects withour a given state in history.
 	 * In addition they need to have a given configurationId and a targetLanguageId.
-	 * 
+	 *
 	 * @param string $state the workflow state.
 	 * @param int $configurationId uid of the configuration record.
 	 * @param int $targetLanguageId uid of the target language record.
@@ -98,7 +73,7 @@ class tx_l10nmgr_models_exporter_exportDataRepository extends tx_mvc_ddd_typo3_a
 					' AND l10ncfg_id = '.intval($configurationId).
 					' AND translation_lang = '.intval($targetLanguageId);
 
-		return $this->findByWhere($where, false, false, false, $add_enable_fields);
+		return $this->findByWhere($where, $add_enable_fields, false, false, false);
 	}
 }
 
