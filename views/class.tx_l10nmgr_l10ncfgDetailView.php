844acadcc5e99a3d23b637af09ad43a147231417
timo_aoemedia <timo_aoemedia@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-07-14 09:44:35 +0000

    * changes for issue #9631
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v4@22287 735d13b6-9817-0410-8766-e36946ffe9aa

--- views/class.tx_l10nmgr_l10ncfgDetailView.php
+++ views/class.tx_l10nmgr_l10ncfgDetailView.php
@@ -33,86 +33,86 @@
  * @package TYPO3
  * @subpackage tx_l10nmgr
  */
-//class tx_l10nmgr_l10ncfgDetailView {
-//
-//	protected $l10ncfgObj = array();	// Internal array (=datarow of config record)
-//
-//	/**
-//	 * @var $doc
-//	 */
-//	var $doc = null;
-//	
-//	/**
-//	* constructor. Set the internal required objects as paramter in constructor (kind of dependency injection, and communicate the dependencies)
-//	* @param tx_l10nmgr_models_configuration_configuration	
-//	**/
-//	function tx_l10nmgr_l10ncfgDetailView($l10ncfgObj, $doc) {
-//		$this->l10ncfgObj=&$l10ncfgObj;
-//		$this->doc = $doc;
-//	}
-//	
-//	/**
-//	* checks if the internal tx_l10nmgr_models_configuration_configuration object is valid
-//	**/
-//	function _hasValidConfig() {
-//		if ($this->l10ncfgObj instanceof tx_l10nmgr_models_configuration_configuration && $this->l10ncfgObj->getId() != 0) {
-//			return true;
-//		}
-//		else  {
-//			return false;
-//		}
-//	}
-//	/**
-//	* returns HTML table with infos for the l10nmgr config.
-//	*	(needs valid configuration to be set)
-//	**/
-//	function render()	{
-//		global $LANG;
-//		$content = '';
-//
-//		if (!$this->_hasValidConfig()) {
-//			return $LANG->getLL('general.export.configuration.error.title');
-//		}
-//
-//		$configurationSettings = '
-//			
-//			<a href="javascript:toggle_visibility(\'l10ncfgOverview\')">'.$LANG->getLL('general.export.configuration.show.title').'</a>
-//			<div id="l10ncfgOverview" style="display:none;"><h2>'.
-//				$LANG->getLL('general.export.configuration.title').'</h2>
-//				<table border="1" cellpadding="1" cellspacing="0" width="400">
-//					<tr class="bgColor5 tableheader">
-//						<td colspan="4"><strong>'.htmlspecialchars($this->l10ncfgObj->getData('title')).' ['.$this->l10ncfgObj->getData('uid').']</strong></td>
-//					</tr>
-//					<tr class="bgColor3">
-//						<td><strong>'.$LANG->getLL('general.list.headline.depth.title').':</strong></td>
-//						<td>'.htmlspecialchars($this->l10ncfgObj->getData('depth')).'&nbsp;</td>
-//						<td><strong>'.$LANG->getLL('general.list.headline.tables').':</strong></td>
-//						<td>'.htmlspecialchars($this->l10ncfgObj->getData('tablelist')).'&nbsp;</td>
-//					</tr>
-//					<tr class="bgColor3">
-//						<td><strong>'.$LANG->getLL('general.list.headline.exclude.title').':</strong></td>
-//						<td>'.htmlspecialchars($this->l10ncfgObj->getData('exclude')).'&nbsp;</td>
-//						<td><strong>'.$LANG->getLL('general.list.headline.include.title').':</strong></td>
-//						<td>'.htmlspecialchars($this->l10ncfgObj->getData('include')).'&nbsp;</td>
-//					</tr>
-//				</table>
-//			</div>
-//				';
-//
-//		$content .= $this->doc->section('', $configurationSettings);
-//
-//		return $content;
-//
-//	}
-//	
-//	
-//}
-//
-//
-//
-//
-//if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_l10ncfgDetailView.php'])	{
-//	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_l10ncfgDetailView.php']);
-//}
-//
-//?>
+class tx_l10nmgr_l10ncfgDetailView {
+
+	protected $l10ncfgObj = array();	// Internal array (=datarow of config record)
+
+	/**
+	 * @var $doc
+	 */
+	var $doc = null;
+	
+	/**
+	* constructor. Set the internal required objects as paramter in constructor (kind of dependency injection, and communicate the dependencies)
+	* @param tx_l10nmgr_models_configuration_configuration	
+	**/
+	function tx_l10nmgr_l10ncfgDetailView($l10ncfgObj, $doc) {
+		$this->l10ncfgObj=&$l10ncfgObj;
+		$this->doc = $doc;
+	}
+	
+	/**
+	* checks if the internal tx_l10nmgr_models_configuration_configuration object is valid
+	**/
+	function _hasValidConfig() {
+		if ($this->l10ncfgObj instanceof tx_l10nmgr_models_configuration_configuration && $this->l10ncfgObj->getId() != 0) {
+			return true;
+		}
+		else  {
+			return false;
+		}
+	}
+	/**
+	* returns HTML table with infos for the l10nmgr config.
+	*	(needs valid configuration to be set)
+	**/
+	function render()	{
+		global $LANG;
+		$content = '';
+
+		if (!$this->_hasValidConfig()) {
+			return $LANG->getLL('general.export.configuration.error.title');
+		}
+
+		$configurationSettings = '
+			
+			<a href="javascript:toggle_visibility(\'l10ncfgOverview\')">'.$LANG->getLL('general.export.configuration.show.title').'</a>
+			<div id="l10ncfgOverview" style="display:none;"><h2>'.
+				$LANG->getLL('general.export.configuration.title').'</h2>
+				<table border="1" cellpadding="1" cellspacing="0" width="400">
+					<tr class="bgColor5 tableheader">
+						<td colspan="4"><strong>'.htmlspecialchars($this->l10ncfgObj->getData('title')).' ['.$this->l10ncfgObj->getData('uid').']</strong></td>
+					</tr>
+					<tr class="bgColor3">
+						<td><strong>'.$LANG->getLL('general.list.headline.depth.title').':</strong></td>
+						<td>'.htmlspecialchars($this->l10ncfgObj->getData('depth')).'&nbsp;</td>
+						<td><strong>'.$LANG->getLL('general.list.headline.tables').':</strong></td>
+						<td>'.htmlspecialchars($this->l10ncfgObj->getData('tablelist')).'&nbsp;</td>
+					</tr>
+					<tr class="bgColor3">
+						<td><strong>'.$LANG->getLL('general.list.headline.exclude.title').':</strong></td>
+						<td>'.htmlspecialchars($this->l10ncfgObj->getData('exclude')).'&nbsp;</td>
+						<td><strong>'.$LANG->getLL('general.list.headline.include.title').':</strong></td>
+						<td>'.htmlspecialchars($this->l10ncfgObj->getData('include')).'&nbsp;</td>
+					</tr>
+				</table>
+			</div>
+				';
+
+		$content .= $this->doc->section('', $configurationSettings);
+
+		return $content;
+
+	}
+	
+	
+}
+
+
+
+
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_l10ncfgDetailView.php'])	{
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/l10nmgr/views/class.tx_l10nmgr_l10ncfgDetailView.php']);
+}
+
+?>
