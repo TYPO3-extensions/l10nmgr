538ed9eafbaf91a8a81396c59d6ffb264c0c0a9e
michael.klapper <michael.klapper@735d13b6-9817-0410-8766-e36946ffe9aa>
2009-10-20 12:51:34 +0000

    Remove whitespaces
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v4@25634 735d13b6-9817-0410-8766-e36946ffe9aa

--- service/class.tx_l10nmgr_service_autoload.php
+++ service/class.tx_l10nmgr_service_autoload.php
@@ -3,33 +3,34 @@
 require_once t3lib_extMgm::extPath('l10nmgr') . '/domain/tools/class.tx_l10nmgr_tools.php';
 
 class tx_l10nmgr_service_autoload {
-	
+
 	/**
 	 * @var string
 	 */
 	protected static $extPath;
-	
+
 	/**
 	 * Retrieves the cached extension path;
-	 * 
+	 *
 	 * @param void
 	 * @return string
-	 * 
+	 *
 	 * @author Timo Schmidt
 	 */
 	protected static function getExtPath(){
 		if(!isset(self::$extPath)){
 			self::$extPath =  t3lib_extMgm::extPath('l10nmgr');
 		}
-		
+
 		return self::$extPath;
 	}
+
 	/**
 	 * Autoload method
 	 *
 	 * @param string class name
 	 * @return bool
-	 * 
+	 *
 	 * @author Timo Schmidt
 	 */
 	public static function autoLoad($className) {
