4f840970369c5ccfa3cce69aedfa005290011d23
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-07-30 08:40:56 +0000

    [~FEATURE] Add option to enable / disable the use of status icons in pre 4.5 pagetree.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/trunk@50302 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -16,6 +16,7 @@ define('L10NMGR_FILEVERSION','1.1');
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['lowlevel']['cleanerModules']['tx_l10nmgr_index'] = array('EXT:l10nmgr/class.tx_l10nmgr_index.php:tx_l10nmgr_index');
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_l10nmgr'] = 'EXT:l10nmgr/class.l10nmgr_tcemain_hook.php:&tx_l10nmgr_tcemain_hook';
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['recStatInfoHooks']['tx_l10nmgr'] = 'EXT:l10nmgr/class.l10nmgr_tcemain_hook.php:&tx_l10nmgr_tcemain_hook->stat';
-
+if ($_EXTCONF['enable_stat_hook']) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['recStatInfoHooks']['tx_l10nmgr'] = 'EXT:l10nmgr/class.l10nmgr_tcemain_hook.php:&tx_l10nmgr_tcemain_hook->stat';
+}
 ?>
