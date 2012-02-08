7c87c6f947931fe991a6eea2004a951027bc74ef
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-02-08 15:40:28 +0000

    [~FEATURE] Fixes #33522. Raise TYPO3 version compatibility to 4.6.99. [RELEASE] Version 3.3.15
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/trunk@57471 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Module for managing localization import and export',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '3.3.13',
+	'version' => '3.3.14',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.4.0-4.5.99',
+			'typo3' => '4.4.0-4.6.99',
 		),
 		'conflicts' => array(
 		),
