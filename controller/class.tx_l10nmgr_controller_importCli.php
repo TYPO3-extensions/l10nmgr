de9f671a37374a8959e318ab34a9f3838292178f
tolleiv <tolleiv@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-15 09:49:40 +0000

    Whitespaces changes - Patch by StefanR.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/branches/v4@75357 735d13b6-9817-0410-8766-e36946ffe9aa

--- controller/class.tx_l10nmgr_controller_importCli.php
+++ controller/class.tx_l10nmgr_controller_importCli.php
@@ -24,7 +24,7 @@
  *  This copyright notice MUST APPEAR in all copies of the script!
  ***************************************************************/
 
-require_once(t3lib_extMgm::extPath('mvc').'mvc/controller/class.tx_mvc_controller_cli.php');
+require_once(t3lib_extMgm::extPath('mvc') . 'mvc/controller/class.tx_mvc_controller_cli.php');
 
 
 /**
@@ -81,7 +81,6 @@ class tx_l10nmgr_controller_importCli extends tx_mvc_controller_cli {
 		),
 	);
 
-
 	/**
 	 * Inizialize the backend user
 	 *
@@ -90,79 +89,68 @@ class tx_l10nmgr_controller_importCli extends tx_mvc_controller_cli {
 	 */
 	public function initializeController() {
 
-		// Force user to admin state
+			// Force user to admin state
 		$GLOBALS['BE_USER']->user['admin'] = 1;
-
-		// Set workspace to the required workspace ID from CATXML:
-		// $GLOBALS['BE_USER']->setWorkspace();
 	}
 
 	/**
 	 * Default action
 	 *
-	 * @param void
+	 * @throws tx_mvc_exception_invalidArgument
 	 * @return void;
 	 */
 	public function defaultAction() {
 
 		if (isset($this->arguments['--help']) || isset($this->arguments['-h']) || !isset($this->arguments['--file']) ) {
-
 			return $this->routeToAction('helpAction');
+		}
 
-		} else {
+		$destinationFolder = t3lib_div::getFileAbsFileName(tx_mvc_common_typo3::getTCAConfigValue('uploadfolder', tx_l10nmgr_domain_importer_importFile::getTableName(), 'filename'));
 
-			$destinationFolder = t3lib_div::getFileAbsFileName(tx_mvc_common_typo3::getTCAConfigValue('uploadfolder', tx_l10nmgr_domain_importer_importFile::getTableName(), 'filename'));
+		foreach ($this->arguments['--file'] as $file) {
 
-			foreach ($this->arguments['--file'] as $file) {
-
-				tx_mvc_validator_factory::getFileValidator()->isValid($file, true);
+			tx_mvc_validator_factory::getFileValidator()->isValid($file, TRUE);
 
 				// create importData record
-				$importData = new tx_l10nmgr_domain_importer_importData();
-				$importData['exportdata_id'] = 0; // will be updated later
-				$importData['configuration_id'] = 0; // will be updated later
-				$importData['import_type'] = 'xml';
+			$importData = new tx_l10nmgr_domain_importer_importData();
+			$importData['exportdata_id'] = 0; // will be updated later
+			$importData['configuration_id'] = 0; // will be updated later
+			$importData['import_type'] = 'xml';
 
-				$importDataRepository = new tx_l10nmgr_domain_importer_importDataRepository();
-				$importDataRepository->add($importData);
+			$importDataRepository = new tx_l10nmgr_domain_importer_importDataRepository();
+			$importDataRepository->add($importData);
 
-				$pathInfo = pathinfo($file);
+			$pathInfo = pathinfo($file);
 
 				// prepend the importdata uid to the new file name
-				$destinationFileName = $importData->getUid() . '_' . $pathInfo['basename'];
+			$destinationFileName = $importData->getUid() . '_' . $pathInfo['basename'];
 
-				$destinationFile = $destinationFolder . DIRECTORY_SEPARATOR . $destinationFileName;
+			$destinationFile = $destinationFolder . DIRECTORY_SEPARATOR . $destinationFileName;
 
 				// move to upload folder
-				if(!copy($file, $destinationFile)){
-					throw new tx_mvc_exception_invalidArgument('Can\'t copy '.$file.' to '.$destinationFile);
-				}
-
-				$this->cli_echo(sprintf('Copying file "%s" to "%s"'.chr(10), $file, $destinationFile));
-
-				t3lib_div::fixPermissions($destinationFile);
-
-				$importFile = new tx_l10nmgr_domain_importer_importFile();
-				$importFile['importdata_id'] = $importData->getUid();
-				$importFile['filename'] = $destinationFileName;
-
-				$importFileRepository = new tx_l10nmgr_domain_importer_importFileRepository();
-				$importFileRepository->add($importFile);
-
-				$this->cli_echo('Extracting zip content...');
-				$importData->extractAllZipContent();
-				$this->cli_echo(" done!\n");
-
-				do {
-					$this->cli_echo(sprintf('%s%% finished'."\n", round($importData->getProgressPercentage())));
-				} while (!tx_l10nmgr_domain_importer_importer::performImportRun($importData));
-				$this->cli_echo(sprintf('%s%% finished'."\n", round($importData->getProgressPercentage())));
+			if (!copy($file, $destinationFile)) {
+				throw new tx_mvc_exception_invalidArgument('Can\'t copy ' . $file . ' to ' . $destinationFile);
 			}
 
+			$this->cli_echo(sprintf('Copying file "%s" to "%s"' . chr(10), $file, $destinationFile));
+
+			t3lib_div::fixPermissions($destinationFile);
+
+			$importFile = new tx_l10nmgr_domain_importer_importFile();
+			$importFile['importdata_id'] = $importData->getUid();
+			$importFile['filename'] = $destinationFileName;
+
+			$importFileRepository = new tx_l10nmgr_domain_importer_importFileRepository();
+			$importFileRepository->add($importFile);
+
+			$this->cli_echo('Extracting zip content...');
+			$importData->extractAllZipContent();
+			$this->cli_echo(" done!\n");
+
+			do {
+				$this->cli_echo(sprintf('%s%% finished' . "\n", round($importData->getProgressPercentage())));
+			} while (!tx_l10nmgr_domain_importer_importer::performImportRun($importData));
+			$this->cli_echo(sprintf('%s%% finished' . "\n", round($importData->getProgressPercentage())));
 		}
 	}
-
-
 }
-
-?>
