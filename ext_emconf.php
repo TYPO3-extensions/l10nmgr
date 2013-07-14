9fb85f69cf8fa4db876daba1f5e4624217445f27
andreas <andreas@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-07-14 14:54:42 +0000

    * [TASK] Changes version number to 3.4.50.
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/l10nmgr/trunk@77540 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -15,7 +15,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'description' => 'Module for managing localization import and export',
 	'category' => 'module',
 	'shy' => 0,
-	'version' => '3.4.0',
+	'version' => '3.4.50',
 	'dependencies' => '',
 	'conflicts' => '',
 	'priority' => '',
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '4.4.0-4.6.99',
+			'typo3' => '4.5.0-4.7.12',
 		),
 		'conflicts' => array(
 		),
