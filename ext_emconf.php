dcbc28e77d28cc7e536afc3fa87f8cd7febf2916
Jo Hasenau <info@cybercraft.de>
2016-12-08 10:07:57 +0100

    [TASK] Adjusted version numbers to start with CMS 8 development
    Change-Id: I7745286341a08213c7db6f4a4dc24ae46ff80e92
    Reviewed-on: https://review.typo3.org/50898
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

--- ext_emconf.php
+++ ext_emconf.php
@@ -33,7 +33,7 @@ $EM_CONF[$_EXTKEY] = array(
     'constraints' => array(
         'depends' => array(
             'php' => '5.5.0-0.0.0',
-            'typo3' => '7.6.0-7.6.99',
+            'typo3' => '7.6.0-8.99.99',
             'static_info_tables' => '6.3.0-0.0.0'
         ),
         'conflicts' => array(),
