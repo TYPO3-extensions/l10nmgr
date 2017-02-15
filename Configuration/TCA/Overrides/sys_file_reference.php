2e71630610f2681d32d95bfc504ab1a9b8318170
Jo Hasenau <info@cybercraft.de>
2017-02-15 16:41:11 +0100

    [TASK] Remove sys_file_reference relation fields from export
    These fields are originally configured as input fields, which they
    actually aren't, since the just hold mm relation information
    
    Change-Id: If3b2c468233c875b4cd44cbd79aa2c86a8779ed4
    Reviewed-on: https://review.typo3.org/51702
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>

---
+++ Configuration/TCA/Overrides/sys_file_reference.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TCA']['sys_file_reference']['columns']['tablenames']['config'] = array('type' => 'passthrough');
+$GLOBALS['TCA']['sys_file_reference']['columns']['fieldname']['config'] = array('type' => 'passthrough');
+$GLOBALS['TCA']['sys_file_reference']['columns']['table_local']['config'] = array('type' => 'passthrough');
