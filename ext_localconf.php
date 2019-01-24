bd01dfce0a2d148bd1f504e1976fe405d32b25d9
Cybercraft <info@cybercraft.de>
2019-01-24 11:21:32 +0100

    [BUGFIX] Add missing namespaces
    Change-Id: Ia0780423f569271f7685f5d9006425f1bae02d59
    Reviewed-on: https://review.typo3.org/59556
    Reviewed-by: Jo Hasenau <info@cybercraft.de>
    Tested-by: Jo Hasenau <info@cybercraft.de>
    (cherry picked from commit 75067e67a4eab727a22d1644bd6192d184d2d984)
    Reviewed-on: https://review.typo3.org/59559

--- ext_localconf.php
+++ ext_localconf.php
@@ -20,8 +20,8 @@ if (TYPO3_MODE == 'BE') {
 //! increase with every change to XML Format
 define('L10NMGR_FILEVERSION', '1.2');
 define('L10NMGR_VERSION', '7.0.0');
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['lowlevel']['cleanerModules']['tx_l10nmgr_index'] = array('EXT:l10nmgr/Classes/Index.php:Index');
-$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_l10nmgr'] = 'EXT:l10nmgr/Classes/Hooks/Tcemain.php:Tcemain';
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['lowlevel']['cleanerModules']['tx_l10nmgr_index'] = array(\Localizationteam\L10nmgr\Index::class);
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['processDatamapClass']['tx_l10nmgr'] = \Localizationteam\L10nmgr\Hooks\Tcemain::class;
 $_EXTCONF_ARRAY = unserialize($_EXTCONF);
 if ($_EXTCONF_ARRAY['enable_stat_hook']) {
   $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['GLOBAL']['recStatInfoHooks']['tx_l10nmgr'] = 'EXT:l10nmgr/Classes/Hooks/Tcemain.php:Tcemain->stat';
